d4bdb5fe11f29ddf19d0e94cef35d55708451595
ThiliniYA <thiliniyapa88@gmail.com>
2026-08-09 23:50:27 +1200

    Add Recipes and Register navigation tests using HomePage POM

---
+++ automation/HealthyBites.Automation/Pages/HomePage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace HealthyBites.Automation.Pages
+{
+    public class HomePage
+    {
+        private readonly IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void ClickRecipesLink()
+        {
+            driver.FindElement(By.LinkText("Recipes")).Click();
+        }
+
+        public void ClickRegisterLink()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+        }
+    }
+}
--- automation/HealthyBites.Automation/Tests/HomePageTests.cs
+++ automation/HealthyBites.Automation/Tests/HomePageTests.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using HealthyBites.Automation.Drivers;
 using HealthyBites.Automation.Config;
+using HealthyBites.Automation.Pages;
 
 namespace HealthyBites.Automation.Tests
 {
@@ -9,20 +10,37 @@ namespace HealthyBites.Automation.Tests
     public class HomePageTests
     {
         private IWebDriver driver = null!;
+        private HomePage homePage = null!;
 
         [SetUp]
         public void SetUp()
         {
             var driverFactory = new DriverFactory("chrome");
             driver = driverFactory.Driver;
+            homePage = new HomePage(driver);
+            driver.Navigate().GoToUrl(TestConfig.GetPageUrl("index.html"));
         }
 
         [Test]
         public void TC001_VerifyHomePageLoads()
         {
-            driver.Navigate().GoToUrl(TestConfig.GetPageUrl("index.html"));
-
             Assert.That(driver.Title, Is.EqualTo("HEALTHY BITES - Home"));
+        }
+
+        [Test]
+        public void TC003_VerifyRecipesLinkNavigates()
+        {
+            homePage.ClickRecipesLink();
+
+            Assert.That(driver.Title, Is.EqualTo("HEALTHY BITES - Recipes"));
+        }
+
+        [Test]
+        public void TC004_VerifyRegisterLinkNavigates()
+        {
+            homePage.ClickRegisterLink();
+
+            Assert.That(driver.Title, Is.EqualTo("HEALTHY BITES - Register"));
         }
 
         [TearDown]
