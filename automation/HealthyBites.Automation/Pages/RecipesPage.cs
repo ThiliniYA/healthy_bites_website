983c794218a0a542ad10100120987f49cad757bd
ThiliniYA <thiliniyapa88@gmail.com>
2026-08-10 00:18:49 +1200

    Add Recipes page tests for content and navigation

---
+++ automation/HealthyBites.Automation/Pages/RecipesPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+
+namespace HealthyBites.Automation.Pages
+{
+    public class RecipesPage
+    {
+        private readonly IWebDriver driver;
+
+        public RecipesPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public ReadOnlyCollection<IWebElement> GetRecipeRows()
+        {
+            // Skips the header row, returns just the data rows
+            return driver.FindElements(By.CssSelector("table tbody tr"));
+        }
+
+        public void ClickHomeLink()
+        {
+            driver.FindElement(By.LinkText("Home")).Click();
+        }
+    }
+}
---
+++ automation/HealthyBites.Automation/Tests/RecipesPageTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using HealthyBites.Automation.Drivers;
+using HealthyBites.Automation.Config;
+using HealthyBites.Automation.Pages;
+
+namespace HealthyBites.Automation.Tests
+{
+    [TestFixture]
+    public class RecipesPageTests
+    {
+        private IWebDriver driver = null!;
+        private RecipesPage recipesPage = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var driverFactory = new DriverFactory("chrome");
+            driver = driverFactory.Driver;
+            recipesPage = new RecipesPage(driver);
+            driver.Navigate().GoToUrl(TestConfig.GetPageUrl("recipes.html"));
+        }
+
+        [Test]
+        public void TC006_VerifyRecipesPageLoads()
+        {
+            Assert.That(driver.Title, Is.EqualTo("HEALTHY BITES - Recipes"));
+        }
+
+        [Test]
+        public void TC007_VerifyRecipeTableHasExpectedRows()
+        {
+            var rows = recipesPage.GetRecipeRows();
+
+            Assert.That(rows.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TC009_VerifyHomeLinkNavigatesBack()
+        {
+            recipesPage.ClickHomeLink();
+
+            Assert.That(driver.Title, Is.EqualTo("HEALTHY BITES - Home"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+        }
+    }
+}
